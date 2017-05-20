e7c0c1791368591d9af167e600fac2e06d30eec9
Brendan Jennings <jbrendan70@outlook.com>
2017-05-20 15:24:49 +0100

    changed to string

--- modzillainst.py
+++ modzillainst.py
@@ -10,7 +10,7 @@ import fileinput
 #file.close()
 
 #Start editing method 1
-hostingurl = int(input("What is the hosting url for your bot (this is where the bot will say the address is. Include an address and port): "))
+hostingurl = str(input("What is the hosting url for your bot (this is where the bot will say the address is. Include an address and port): "))
 for line in fileinput.input("Configuration\config.json", inplace=True):
     print "hostingurl" % (fileinput.filelineno(), 4),
 
